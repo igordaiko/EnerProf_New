a7ad33219c80fe81b4d7a3aa8cb88f1c8a9668fa
igordaiko <id_inworld@hotmail.com>
2016-09-04 18:08:01 +0300

    blya

---
+++ EnerProf/Models/Clients.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+namespace EnerProf.Models
+{
+    public class Clients
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Название компании клиента")]
+        [Required(ErrorMessage = "Пожалуйста, введите название")]
+        public string Name { get; set; }
+        [Display(Name = "Изображение")]
+        public string Img { get; set; }
+    }
+}
---
+++ EnerProf/Models/Industries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EnerProf.Models
+{
+    public class Industries
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+        [Column(TypeName = "ntext")]
+        public string Description { get; set; }
+    }
+}
